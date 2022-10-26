b001dee30f30e501243edac2d591c86719df6e4b
pedohpt <pedro1112hpt@gmail.com>
2022-10-25 21:53:23 -0300

    exercicio 3

--- Exercicio3.py
+++ Exercicio3.py
@@ -1,7 +1,17 @@
 print("Digete o primeiro numero")
 
-num1 = input(num1)
+num1 = input(int())
 
 print("Digite o segundo numero")
 
-num2 = input(num2)
+num2 = input(int())
+
+soma = int(num1) + int(num2)
+subtracao = int(num1) - int(num2)
+multiplicacao = int(num1) * int(num2)
+divisao = int(num1) / int(num2)
+
+print("A soma dos numeros é: " + str(soma))
+print("A subtração dos numeros é: " + str(subtracao))
+print("A multiplicação dos numeros é: " + str(multiplicacao))
+print("A divisao dos numeros é: " + str(divisao))
